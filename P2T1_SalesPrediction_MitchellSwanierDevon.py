383be52a3fd1ef44b91d5aa206f8407197ca2ad2
Vonnyy <62961238+Vonnyy@users.noreply.github.com>
2020-03-31 15:18:17 -0400

    First submission

---
+++ P2T1_SalesPrediction_MitchellSwanierDevon.py
@@ -0,0 +1,17 @@
+# This is a source code of how to calculate the prediction of total sales
+#3/30/2020
+#CTI-110 P2T1
+#Devon Mitchell-Swanier
+
+
+
+# Get the projected total sales
+total_sales =float(input('Enter the projected sales:  '))
+
+# Calculate the profit as 23 percent of Total sales.
+profit =total_sales * 0.23
+
+#Display the Profit
+print('The profit is $', profit)
+
+
